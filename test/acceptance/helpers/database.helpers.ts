0c3f6e90fd0c06d2188c58d746f68edfa9fa1b24
Daniel J. B. Clarke <u8sand@gmail.com>
2018-11-08 12:46:33 -0500

    Get valid/invalid data from schema tests

--- test/acceptance/helpers/database.helpers.ts
+++ test/acceptance/helpers/database.helpers.ts
@@ -1,3 +1,6 @@
+import * as entityTest from "@dcic/signature-commons-schema/core/entity.test.json";
+import * as libraryTest from "@dcic/signature-commons-schema/core/library.test.json";
+import * as signatureTest from "@dcic/signature-commons-schema/core/signature.test.json";
 import { Entity, Library, Signature, UserProfile } from "../../../src/models";
 import { EntityRepository, LibraryRepository, SignatureRepository } from "../../../src/repositories";
 import { testdb } from "../fixtures/datasources/testdb.datasource";
@@ -5,41 +8,14 @@ import { UserProfileRepository } from "../../../src/repositories/user-profile.re
 
 export async function givenValidLibraryData(data?: Partial<Library>) {
   return Object.assign(
-    {
-      id: 0,
-      uuid: 'ee4ff944-17de-4715-889e-c51f7d5e8cde',
-      meta: <any>{
-        $validator: {
-          properties: {
-            test: {
-              type: 'string'
-            }
-          },
-          required: ['test']
-        },
-        test: 'test'
-      }
-    },
+    libraryTest.tests.filter((test) => test.valid)[0].data,
     data,
   )
 }
 
 export async function givenInvalidLibraryData(data?: Partial<Library>) {
   return Object.assign(
-    {
-      id: 0,
-      uuid: 'ee4ff944-17de-4715-889e-c51f7d5e8cde',
-      meta: <any>{
-        $validator: {
-          properties: {
-            test: {
-              type: 'string'
-            }
-          },
-          required: ['test']
-        }
-      }
-    },
+    libraryTest.tests.filter((test) => !test.valid)[0].data,
     data,
   )
 }
@@ -54,41 +30,14 @@ export async function givenLibrary(data?: Partial<Library>) {
 
 export async function givenValidSignatureData(data?: Partial<Signature>) {
   return Object.assign(
-    {
-      id: 1,
-      uuid: 'd6cdda1b-fd03-45b3-af91-a2de6ad26614',
-      meta: {
-        $validator: {
-          properties: {
-            test: {
-              type: 'string'
-            }
-          },
-          required: ['test']
-        },
-        test: 'test'
-      }
-    },
+    signatureTest.tests.filter((test) => test.valid)[0].data,
     data,
   )
 }
 
 export async function givenInvalidSignatureData(data?: Partial<Signature>) {
   return Object.assign(
-    {
-      id: 1,
-      uuid: 'd6cdda1b-fd03-45b3-af91-a2de6ad26614',
-      meta: {
-        $validator: {
-          properties: {
-            test: {
-              type: 'string'
-            }
-          },
-          required: ['test']
-        }
-      }
-    },
+    signatureTest.tests.filter((test) => !test.valid)[0].data,
     data,
   )
 }
@@ -103,42 +52,15 @@ export async function givenSignature(data?: Partial<Signature>) {
 
 export async function givenValidEntityData(data?: Partial<Entity>) {
   return Object.assign(
-    {
-      id: 2,
-      // uuid: '8d098857-7167-4f1d-a8f9-7389bf578e67',
-      meta: {
-        $validator: {
-          properties: {
-            test: {
-              type: 'string'
-            }
-          },
-          required: ['test']
-        },
-        test: 'test'
-      }
-    },
-    data
+    entityTest.tests.filter((test) => test.valid)[0].data,
+    data,
   )
 }
 
 export async function givenInvalidEntityData(data?: Partial<Entity>) {
   return Object.assign(
-    {
-      id: 2,
-      // uuid: '8d098857-7167-4f1d-a8f9-7389bf578e67',
-      meta: {
-        $validator: {
-          properties: {
-            test: {
-              type: 'string'
-            }
-          },
-          required: ['test']
-        }
-      }
-    },
-    data
+    entityTest.tests.filter((test) => !test.valid)[0].data,
+    data,
   )
 }
 
