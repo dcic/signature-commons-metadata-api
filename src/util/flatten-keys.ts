ad6f174c6f319ed6237dbc267db61b2aceed7cb7
Daniel J. B. Clarke <u8sand@gmail.com>
2019-03-07 17:50:18 -0500

    Added flatten_keys, for turning nested dicts into flat dicts

---
+++ src/util/flatten-keys.ts
@@ -0,0 +1,30 @@
+export function flatten_keys(obj: object) {
+  const new_obj: any = { ...obj }
+  const done = new Set()
+  let changed = true
+  while (changed) {
+    changed = false
+
+    for (const key of Object.keys(new_obj)) {
+      if (done.has(key)) continue
+
+      if (Array.isArray(new_obj[key])) {
+        new_obj[key] = {
+          inq: new_obj[key]
+        }
+        changed = true
+        done.add(key)
+      } else if (typeof new_obj[key] === 'object') {
+        for (const k of Object.keys(new_obj[key])) {
+          new_obj[`${key}.${k}`] = new_obj[key][k]
+          changed = true
+        }
+        delete new_obj[key]
+      } else {
+        changed = true
+        done.add(key)
+      }
+    }
+  }
+  return new_obj
+}
---
+++ test/unit/util/flatten-keys.unit.ts
@@ -0,0 +1,33 @@
+import * as assert from 'assert'
+import { flatten_keys } from '../../../src/util/flatten-keys'
+
+describe('util', () => {
+  describe('flatten_keys', () => {
+    it('works', () => {
+      assert.deepEqual(
+        flatten_keys({
+          id: '1',
+          meta: {
+            test: {
+              me: ['plenty', 'always'],
+              and: 2
+            },
+            best: {
+              nest: {
+                guest: '3'
+              }
+            }
+          }
+        }),
+        {
+          id: '1',
+          'meta.test.me': {
+            inq: ['plenty', 'always'],
+          },
+          'meta.test.and': 2,
+          'meta.best.nest.guest': '3'
+        }
+      )
+    })
+  })
+})
