ed40885575d2be46a16cbe4f987cb62efe5256a6
John Erol Evangelista <je.evangelista04@gmail.com>
2019-10-23 10:23:24 -0400

    Removed signature_keys from library

--- src/entities/library.model.ts
+++ src/entities/library.model.ts
@@ -79,18 +79,6 @@ export class Library extends LBEntity {
     [key: string]: any
   };
 
-  @property({
-    type: 'array',
-    itemType: 'string',
-    required: false,
-  })
-  @Column({
-    name: 'signature_keys',
-    type: 'jsonb',
-    default: [],
-  })
-  signature_keys: JSON;
-
   @ManyToOne(type => Resource, resource => resource._libraries)
   @JoinColumn({
     name: 'resource',
