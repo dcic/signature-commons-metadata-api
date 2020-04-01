901e0654f32960624e74a20da2cdb5ddf16e7800
Daniel J. B. Clarke <u8sand@gmail.com>
2020-04-01 11:59:36 -0400

    Also refresh summary on startup

--- src/observers/startup.ts
+++ src/observers/startup.ts
@@ -1,12 +1,14 @@
 import { inject } from '@loopback/context';
 import { LifeCycleObserver } from '@loopback/core';
 import { repository } from '@loopback/repository';
+import { SummaryController } from '../controllers/summary.controller';
 import { TypeORMDataSource } from '../datasources';
 import { UserProfileRepository } from '../repositories';
 
 export class StartupObserver implements LifeCycleObserver {
   constructor(
     @repository(UserProfileRepository) private userRepo: UserProfileRepository,
+    @inject('controllers.SummaryController') private summaryController: SummaryController,
     @inject('datasources.typeorm') private typeorm: TypeORMDataSource
   ) {}
 
@@ -30,6 +32,9 @@ export class StartupObserver implements LifeCycleObserver {
     if (process.env.REFRESH_ON_STARTUP === 'true') {
       console.log(`[INFO]: Refreshing materialized views...`);
       await this.typeorm.refresh_materialized_views()
+
+      console.log(`[INFO]: Refreshing summary...`)
+      await this.summaryController.refresh()
     } else {
       console.log('[INFO]: Set REFRESH_ON_STARTUP to true if you want to keep things up to date')
     }
